3def132abaa1a91ca6b448dc45b40c782dc9e2c5
indra-poudel <future.technology1998@gmail.com>
2020-09-06 23:05:03 +0545

    Updates

---
+++ precache-manifest.789df1a5a996df26dbeaaa42b2bf9612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008724fb0c7a2a42dbe6320af18503ba",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "1842fdce5e0d3f21e06d",
+    "url": "/todo-app/static/css/main.dd45cc31.chunk.css"
+  },
+  {
+    "revision": "fde2e46649b052af147e",
+    "url": "/todo-app/static/js/2.b9c31df2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.b9c31df2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1842fdce5e0d3f21e06d",
+    "url": "/todo-app/static/js/main.84bc2fa0.chunk.js"
+  },
+  {
+    "revision": "cad3620733bc5b62f061",
+    "url": "/todo-app/static/js/runtime-main.bb63ee44.js"
+  }
+]);
